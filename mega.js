d36e9b808d757e58e7cb497060edfba535c03d29
PATRON <137694181+Itzpatron@users.noreply.github.com>
2025-04-25 17:23:16 +0100

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '', // Your Mega A/c Email Here
-    password: '', // Your Mega A/c Password Here
+    email: 'maximusfadare@gmail.com', // Your Mega A/c Email Here
+    password: 'Maximus0000.', // Your Mega A/c Password Here
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
