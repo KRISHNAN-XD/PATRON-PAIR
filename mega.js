562b38a922daffb8f305a2b5832b9487b8bfd555
KRISHNAN-XD <monjannick4444@gmail.com>
2025-04-26 19:53:05 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'maximusfadare@gmail.com', // Your Mega A/c Email Here
-    password: 'Maximus0000.', // Your Mega A/c Password Here
+    email: 'monjannick4444@gmail.com', // Your Mega A/c Email Here
+    password: 'achuakhil2255.', // Your Mega A/c Password Here
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
